4cfbc34e75fcb52252491dac7add0df68c13b810
guillar <robin.guillaume-castel@legos.obs-mip.fr>
2024-07-02 09:08:47 +0200

    remove test

--- test.py
+++
@@ -1,6 +0,0 @@
-from cmipspirit.CMIP_path_search import get_path_CMIP5_data_esgf
-
-print(get_path_CMIP5_data_esgf('IPSL-CM5A-LR','piControl', 'r1i1p1', 'areacello'))
-
-from pyesgf.search import SearchConnection
-conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
