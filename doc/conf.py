d6fc0c5372bb7bfa0f4380f67d23f6d94d267682
guillar <robin.guillaume-castel@legos.obs-mip.fr>
2024-07-04 15:42:41 +0200

    doc test

--- doc/conf.py
+++ doc/conf.py
@@ -3,6 +3,12 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys, os
+import datetime
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../lenapy'))
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
