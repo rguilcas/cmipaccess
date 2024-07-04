e2b7095c3fa7c4f2ac6d1567e5b3b73ff9fe8fce
guillar <robin.guillaume-castel@legos.obs-mip.fr>
2024-07-04 15:44:58 +0200

    doc test

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,7 @@ copyright = '2024, Robin Guillaume-Castel'
 author = 'Robin Guillaume-Castel'
 release = '0.3.0'
 
+
 # import os
 # import sys 
 # sys.path.insert(_index:0, os.path.abspath('..'))
@@ -29,6 +30,24 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary'
 ]
+
+
+
+# autosummaries from source-files : use .. autosummary::
+autosummary_generate = True
+# Inherit docstrings from parent classes
+autodoc_inherit_docstrings = True
+# autodoc don't show __init__ docstring
+autoclass_content = "both"
+# sort class members by their order in the source
+autodoc_member_order = 'bysource'
+
+# show all members of a class in the Methods and Attributes sections automatically
+numpydoc_show_class_members = True
+# create a Sphinx table of contents for the lists of class methods and attributes
+numpydoc_class_members_toctree = True
+# show all inherited members of a class in the Methods and Attributes sections
+numpydoc_show_inherited_class_members = True
 
 
 templates_path = ['_templates']
